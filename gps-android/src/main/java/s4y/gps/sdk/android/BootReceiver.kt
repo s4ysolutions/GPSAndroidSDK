1c7b3d13306bb9de4b5be46ec06b6945d27fd217
dsame <dsame@github.com>
2024-06-14 10:02:44 +0200

    Add BootReceiver (not tested)

---
+++ gps-android/src/main/java/s4y/gps/sdk/android/BootReceiver.kt
@@ -0,0 +1,18 @@
+package s4y.gps.sdk.android
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (Intent.ACTION_BOOT_COMPLETED == intent.action || "android.permission.QUICKBOOT_POWERON" == intent.action) {
+            if (GPSPreferences.isBackground(context)) {
+                val startIntent = Intent(context, GPSUpdatesForegroundService::class.java)
+                // it is assumed service will be configured in Application.onCreate
+                startIntent.action = GPSUpdatesForegroundService.ACTION_START
+                context.startService(startIntent)
+            }
+        }
+    }
+}
