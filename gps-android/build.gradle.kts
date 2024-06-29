9c5b49d1a9e028ae43930c115e1346e845b9b21d
Sergey Dolin <sergey@s4y.solutions>
2024-06-29 23:02:08 +0200

    v3.1.0 add ACCESS_BACKGROUND_LOCATION
    It is necessary to launch service on boot

--- gps-android/build.gradle.kts
+++ gps-android/build.gradle.kts
@@ -59,7 +59,7 @@ publishing {
         create<MavenPublication>("Release") {
             groupId = "solutions.s4y.gps"
             artifactId = "gps-sdk-android"
-            version = "3.0.1"
+            version = "3.1.0"
 
             pom {
                 packaging = "aar"
--- gps-android/src/main/java/s4y/gps/sdk/android/BootReceiver.kt
+++ gps-android/src/main/java/s4y/gps/sdk/android/BootReceiver.kt
@@ -8,10 +8,8 @@ class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (Intent.ACTION_BOOT_COMPLETED == intent.action || "android.permission.QUICKBOOT_POWERON" == intent.action) {
             if (GPSPreferences.keepAlive(context)) {
-                val startIntent = Intent(context, GPSUpdatesForegroundService::class.java)
                 // it is assumed service will be configured in Application.onCreate
-                startIntent.action = GPSUpdatesForegroundService.ACTION_START
-                context.startService(startIntent)
+                GPSUpdatesForegroundService.start(context)
             }
         }
     }
